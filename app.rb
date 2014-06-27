a73ac38b9129308e9060f76b3ff7223f46b4657a
Amer Kamil Zainal <ucabakz@ucl.ac.uk>
2014-06-27 22:26:29 +0000

    Edits app.rb

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ require 'sinatra/base'
 class MyApp < Sinatra::Base
 
 	get '/' do 
-		'Hello world! Everything is finally set up! This is development.'
+		'Hello world! Everything is finally set up! This is development. It really is!'
 	end
 
 end
