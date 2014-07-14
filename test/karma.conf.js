6727942db55f77b7231f6c04cc8828c8c9ca022b
Amer Kamil Zainal <ucabakz@ucl.ac.uk>
2014-07-14 16:18:36 +0100

    Update configuration files.
    Change the base application folder and add new dependencies and
    packages.

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -27,6 +27,8 @@ module.exports = function(config) {
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
+      'bower_components/restangular/dist/restangular.js',
+      'bower_components/lodash/dist/lodash.compat.js',
       'public/scripts/**/*.js',
       'test/unit/**/*.js'
     ],
