e8e8ecf209d92af15d7e8d2610120daaf8b283f0
Amer Kamil Zainal <ucabakz@ucl.ac.uk>
2014-07-14 15:53:46 +0100

    Removed the base /api URI for API requests
    Ideally the API will be served up by a separate server, so using a /api base would be redundant.

--- routes/questions.rb
+++ routes/questions.rb
@@ -1,15 +1,15 @@
 # encoding: UTF-8
 
-get '/api/questions' do
+get '/questions' do
 	format_response(Question.all, request.accept)
 end
 
-get '/api/questions/:id' do
+get '/questions/:id' do
 	question ||= Question.get(params[:id]) || halt(404)
 	format_response(question, request.accept)
 end
 
-post '/api/questions' do
+post '/questions' do
 	body = JSON.parse request.body.read
 	question = Question.create(
 		title: 		body['title'],
@@ -19,7 +19,7 @@ post '/api/questions' do
 	format_response(question, request.accept)
 end
 
-put '/api/questions/:id' do
+put '/questions/:id' do
 	body = JSON.parse request.body.read
 	question ||= Question.get(params[:id]) || halt(404)
 	halt 500 unless question.update(
@@ -29,7 +29,7 @@ put '/api/questions/:id' do
 	format_response(question, request.accept)
 end
 
-delete '/api/questions/:id' do
+delete '/questions/:id' do
 	question ||= Question.get(params[:id]) || halt(404)
 	halt 500 unless question.destroy
 end
