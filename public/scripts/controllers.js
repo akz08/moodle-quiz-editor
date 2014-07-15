f06d189ede454b6746c849ad96631f66bc276de2
Amer Kamil Zainal <ucabakz@ucl.ac.uk>
2014-07-15 14:19:51 +0100

    Implement simple example of communication between Angular.js and Sinatra
    Questions can be added or deleted from the server. All CRUD operations
    are done via the API. Editing values has yet to be implemented.

--- public/scripts/controllers.js
+++ public/scripts/controllers.js
@@ -10,14 +10,41 @@ quizEditorControllers.controller('MainCtrl', ['$scope', 'Restangular',
       'Karma'
     ];
 
+    // set default question type
     $scope.questionType = 'trueFalse';
 
+    // setup base route for questions
     var baseQuestions = Restangular.all('questions');
-    // $scope.data = oneQuestion.getList();
 
-    baseQuestions.getList().then(function(questions) {
-      $scope.data = questions;
-    });
+    // retrieve questions to display
+    $scope.updateQuestions = function() {
+      baseQuestions.getList().then(function(allQuestions) {
+        $scope.questionPreview = allQuestions;
+      });
+    };
+
+    // retrieve questions on page load
+    $scope.updateQuestions();
+
+    $scope.createQuestion = function() {
+      var newMessage = {
+        "title": $scope.questionTitle,
+        "type": $scope.questionType
+      };
+
+      baseQuestions.post(newMessage).then(function(newMsg) {
+
+        $scope.updateQuestions();
+
+      });
+    };
+
+    $scope.deleteQuestion = function(id) {
+      baseQuestions.get(id).then(function(question) {
+        question.remove();
+        $scope.updateQuestions();
+      });
+    };
 
   }]);
 
