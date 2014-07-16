0b829e4d087a7b3a33eb5855390961fb49b1308b
Amer Kamil Zainal <ucabakz@ucl.ac.uk>
2014-07-16 15:33:04 +0100

    Add automatic table creation using DataMapper
    Fixes problem of non-existent tables when using DataMapper.

--- app.rb
+++ app.rb
@@ -2,12 +2,12 @@ require 'rubygems'
 require 'sinatra'
 require 'data_mapper'
 require 'json'
-require 'dm-migrations'
 
 
 DataMapper::Logger.new($stdout, :debug)
 
 DataMapper.setup(:default, 'mysql://root:uclthesis@localhost/moodle_quiz_editor')
+
 
 get '/' do 
 	File.read(File.join('public', 'index.html'))
@@ -19,3 +19,5 @@ require './helpers/init'
 require './routes/init'
 
 DataMapper.finalize
+
+DataMapper.auto_migrate! 
