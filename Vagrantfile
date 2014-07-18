9b14d2776890970ab6a9684a9f0060770fa003c4
Amer Kamil Zainal <ucabakz@ucl.ac.uk>
2014-07-18 22:05:22 +0100

    Revert shell script to run with su
    If it ain't broke, don't fix it.

--- Vagrantfile
+++ Vagrantfile
@@ -6,7 +6,7 @@ VAGRANTFILE_API_VERSION = "2"
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
 
   config.vm.box = "ubuntu/trusty64"
-  config.vm.provision :shell, path: "setup.sh", privileged: "false"
+  config.vm.provision :shell, path: "setup.sh"
 
   config.vm.network "forwarded_port", guest: 8080, host: 8000
   # for 'grunt serve'
--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-sudo apt-get update
+apt-get update
 
 # Install Git
-sudo apt-get install -y build-essential git-core
+apt-get install -y build-essential git-core
 
 # Install Ruby
 \curl -sSL https://get.rvm.io | bash -s stable
@@ -12,29 +12,29 @@ rvm reload
 rvm install 2.1.2
 
 # Install front-end tools
-sudo apt-get install -y nodejs-legacy npm
+apt-get install -y nodejs-legacy npm
 npm install -g bower
 
 # Install Java for Selenium/Protractor
-sudo apt-get install -y default-jdk
+apt-get install -y default-jdk
 
 # Install PhantomJS dependency
-sudo apt-get install -y libfontconfig
+apt-get install -y libfontconfig
 
 # Prepare MySQL 5.5 credentials
-sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password password uclthesis'
-sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password_again password uclthesis'
+debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password password uclthesis'
+debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password_again password uclthesis'
 
 # Install MySQL 5.5
-sudo apt-get install -y mysql-server-5.5
+apt-get install -y mysql-server-5.5
 # setup default database
 mysql -uroot -puclthesis -e "CREATE DATABASE moodle_quiz_editor;"
 
 # Install DataMapper dependency for dm-mysql-adapter
-sudo apt-get install -y libmysqlclient-dev
+apt-get install -y libmysqlclient-dev
 
 # Install SQLite (testing)
-sudo apt-get install -y libsqlite3-dev
+apt-get install -y libsqlite3-dev
 
 # Install remaining dependencies
 cd /vagrant
