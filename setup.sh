d2660e0219a3294ee267d1c7c85b7dcc2adac43c
Amer Kamil Zainal <ucabakz@ucl.ac.uk>
2014-07-18 17:40:29 +0100

    Fix permissions with Vagrant provisioning
    All shell commands were run with sudo. Super user privileges now called
    on demand.

--- setup.sh
+++ setup.sh
@@ -1,48 +1,45 @@
 #!/usr/bin/env bash
 
-# based on "https://www.digitalocean.com/community/tutorials/how-to-install-ruby-2-1-0-and-sinatra-on-ubuntu-13-with-rvm"
+sudo apt-get update
 
-apt-get update
-apt-get install -y build-essential git-core
+# Install Git
+sudo apt-get install -y build-essential git-core
+
+# Install Ruby
 \curl -sSL https://get.rvm.io | bash -s stable
 source /etc/profile.d/rvm.sh
 rvm reload
 rvm install 2.1.2
-gem install sinatra
 
-# JS stuff
-apt-get install -y nodejs-legacy npm
+# Install front-end tools
+sudo apt-get install -y nodejs-legacy npm
 npm install -g bower
-# npm install -g yo
-# npm install -g generator-angular
-# npm install -g protractor
 
-# Need to install Java for Selenium/Protractor
-apt-get install -y default-jdk
+# Install Java for Selenium/Protractor
+sudo apt-get install -y default-jdk
 
-# if problems persist when using yo
-# bower cache clean
-# npm cache clean
+# Install PhantomJS dependency
+sudo apt-get install -y libfontconfig
 
-debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password password uclthesis'
-debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password_again password uclthesis'
+# Prepare MySQL 5.5 credentials
+sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password password uclthesis'
+sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password_again password uclthesis'
 
-# MySQL
-apt-get install -y mysql-server-5.5
+# Install MySQL 5.5
+sudo apt-get install -y mysql-server-5.5
 # setup default database
 mysql -uroot -puclthesis -e "CREATE DATABASE moodle_quiz_editor;"
 
-# SQLite
-apt-get install -y libsqlite3-dev
+# Install DataMapper dependency for dm-mysql-adapter
+sudo apt-get install -y libmysqlclient-dev
 
-# Datamapper
-apt-get install -y libmysqlclient-dev
+# Install SQLite (testing)
+sudo apt-get install -y libsqlite3-dev
 
-# PhantomJS
-apt-get install -y libfontconfig
-
-# these commands to be run in the /vagrant directory
+# Install remaining dependencies
 cd /vagrant
+
 bundle install
 npm install --no-bin-links
-bower install --allow-root
+bower install --allow-root| xargs echo
+bundle exec rake db:upgrade;
