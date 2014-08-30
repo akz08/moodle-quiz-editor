691dd3f6a89b063d94a1d4f84ef01fc7a8f6301a
Amer Kamil Zainal <ucabakz@ucl.ac.uk>
2014-08-30 13:50:42 +0100

    Update specs
    Update question routes tests to include answers.

--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -3,4 +3,10 @@ FactoryGirl.define do
 		q_name "A question title"
 		q_type "true_false"
 	end 
+
+  factory :answer do
+    a_answer "An answer"
+
+    question 
+  end
 end
--- spec/routes/quizeditor_api_questions_spec.rb
+++ spec/routes/quizeditor_api_questions_spec.rb
@@ -116,4 +116,101 @@ describe "QuizEditor API" do
 
 	end
 
+	describe "GET /questions/:q_id/answers" do 
+		before(:each) do
+			FactoryGirl.create(:question)
+			2.times {FactoryGirl.create(:answer, question_id: 1)}
+			get '/questions/1/answers'
+		end
+
+		let(:resp) { json_parse last_response.body }
+
+		it 'responds successfully' do
+			expect(last_response).to be_ok
+		end
+
+		it 'returns a list of answers' do
+			expect(resp.length).to eq 2
+		end
+	end
+
+	describe "GET /questions/:q_id/answers/:a_id" do
+		before(:each) do
+			FactoryGirl.create(:answer)
+			get '/questions/1/answers/1' 
+		end
+
+		let(:resp) { json_parse last_response.body }
+
+		it 'responds successfully' do 
+			expect(last_response).to be_ok
+		end
+
+		it 'returns an answer' do
+			expect(resp.length).to eq 7
+			expect(resp[:a_answer]).to eq("An answer")
+		end
+	end
+
+	describe "PUT /questions/:q_id/answers/:a_id" do
+		before(:each) do
+			FactoryGirl.create(:answer)
+			put_json('/questions/1/answers/1', {
+					question_id: "1",
+					a_answer: "A new answer",
+					a_fraction: "0.5"
+			})
+		end
+
+		let(:resp) { json_parse last_response.body }
+
+		it 'responds successfully' do
+			expect(last_response).to be_successful
+		end
+
+		it 'returns the changed answer' do
+			expect(resp[:question_id]).to eq(1)
+			expect(resp[:a_answer]).to eq("A new answer")
+			expect(resp[:a_fraction]).to eq("0.5")
+		end
+	end
+
+	describe "POST /questions/:q_id/answers" do
+		before(:each) do
+			FactoryGirl.create(:question)
+			post_json('/questions/1/answers', {
+					question_id: "1",
+					a_answer: "Some answer"
+			})
+		end
+
+		let(:resp) { json_parse last_response.body }
+
+		it 'responds successfully' do
+			expect(last_response).to be_successful
+		end
+
+		it 'returns the posted answer' do
+			expect(resp[:question_id]).to eq(1)
+			expect(resp[:a_answer]).to eq("Some answer")
+		end
+
+	end
+
+	describe "DELETE /questions/:q_id/answers/:a_id" do
+		before(:each) do
+			FactoryGirl.create(:answer)
+			delete '/questions/1/answers/1'
+		end
+
+		it 'responds successfully' do
+			expect(last_response).to be_successful
+		end
+
+		it 'deletes the specified answer' do
+			get '/questions/1/answers/1'
+			expect(last_response.status).to eql(404) 
+		end
+	end
+
 end
